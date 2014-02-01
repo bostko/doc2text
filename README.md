d083f3fb62aedb04c5a1d2a1176f08e022e75b89
Valentin Aitken <bostko@gmail.com>
2014-02-01 16:31:49 +0200

    Clean whitespaces

--- README.md
+++ README.md
@@ -8,4 +8,3 @@ References
 
 * [Open Document Format for Office Applications](http://docs.oasis-open.org/office/v1.2/os/OpenDocument-v1.2-os.html)
 * [Markdown Syntax](http://daringfireball.net/projects/markdown/syntax#link)
-
--- spec/odt_xml_spec.rb
+++ spec/odt_xml_spec.rb
@@ -3,11 +3,6 @@ require 'spec_helper'
 describe Doc2Text::OdtXML do
   context 'SAX' do
     it 'is able to review docs' do
-      # Create our parser
-      parser = Nokogiri::XML::SAX::Parser.new(Doc2Text::OdtXML.new)
-      # Send some XML to the parser
-
-      parser.parse(File.open('testdata/text_styles_unzipped/content_beauty.xml'))
     end
   end
 end

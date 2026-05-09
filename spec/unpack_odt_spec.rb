670dd4179ea4d2b801f0e1f7bd8a6a70c3dacd57
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-05-09 08:42:23 +0300

    Add spec for temp file

--- spec/unpack_odt_spec.rb
+++ spec/unpack_odt_spec.rb
@@ -17,4 +17,23 @@ describe 'odt' do
     rspec_extract_odt
     rspec_extract_odt
   end
+
+  context "when the odt is a temporary file" do
+    it "runs from a temp file" do
+      tempfile = Tempfile.new('text_styles.odt')
+      tempfile.write File.read(File.join 'spec', 'fixtures', 'text_styles.odt')
+      tempfile.rewind
+      tempfile.close
+
+      @odt = Doc2Text::Odt::Document.new tempfile
+      @odt.unpack
+
+      entries = Dir.glob "#{@odt.extract_path}/**/*"
+      mandatory_files = %w(manifest.rdf content.xml settings.xml styles.xml META-INF META-INF/manifest.xml meta.xml mimetype).map { |entry|
+        File.join @odt.extract_path, entry }
+      expect(entries.to_set.subset? mandatory_files.to_set)
+
+      @odt.clean
+    end
+  end
 end
